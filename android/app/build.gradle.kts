74dff74dfb5a6e96c4fcd412ec2d55811440f14d
Vinayak <vinayakg236@gmail.com>
2026-03-24 11:27:35 +0530

    some files are changed and methods updated

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.vins.personal_bahi_khata"
-    compileSdk = 35
+    namespace = "com.vins.bahi_khata"
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -30,12 +30,12 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.vins.personal_bahi_khata"
+        applicationId = "com.vins.bahi_khata"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
         targetSdk = 35 
-        versionCode = 23
+        versionCode = 24
         versionName = "2.3.0"
     }
 
